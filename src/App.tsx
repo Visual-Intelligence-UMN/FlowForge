027e35460ecf1a325c2f9e0a74ac007df580d19c
Pan Hao <pan00342@umn.edu>
2024-11-08 15:15:20 -0600

    Add drag and drop sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import {
   ReactFlow,
   Background,
@@ -7,36 +7,87 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
+  useReactFlow,
   type OnConnect,
+  ReactFlowProvider,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
+import { DnDProvider } from './components/DnDContext';
+import Sidebar from './components/Sidebar';
+import { useDnD } from './components/DnDContext';
 
 import { initialNodes, nodeTypes } from './nodes';
 import { initialEdges, edgeTypes } from './edges';
 
-export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const DnDFlow = () => {
+  const reactFlowWrapper = useRef(null);
+  const {screenToFlowPosition} = useReactFlow();
+  const [type] = useDnD()
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((edges) => addEdge(connection, edges)),
     [setEdges]
   );
 
+  const onDragOver = useCallback((event: any) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback((event: any) => {
+    event.preventDefault();
+    if(!type) return;
+
+    const position = screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+    const newNode = {
+      id: getId(),
+      type,
+      position,
+      data: {label: `${type} node`},
+    };
+    setNodes((nds: any) => nds.concat(newNode));
+  }, [type, screenToFlowPosition]);
+
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    <div className="dndflow">
+      <Sidebar />
+      <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onDragOver={onDragOver}
+          onDrop={onDrop}
+          fitView
+        >
+          <Background />
+          <MiniMap />
+          <Controls />
+        </ReactFlow>
+      </div>
+      
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <ReactFlowProvider>
+      <DnDProvider>
+      <DnDFlow />
+      </DnDProvider>
+    </ReactFlowProvider>
   );
 }
