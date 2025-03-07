c5a30b0cd9c409366d5632cfba42de9347476352
Nicholas (Nick) Hinds <nicholashinds2@gmail.com>
2025-03-07 05:59:40 -0600

    adjust button layout and header styling

--- src/components/panel-input/PanelTaskInput.jsx
+++ src/components/panel-input/PanelTaskInput.jsx
@@ -14,14 +14,17 @@ import {
   MenuItem,
   InputLabel,
 } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { taskList } from "../../patterns/GlobalStates";
 import { useResetTask } from "../../utils/ResetTask";
+
 function TaskPanel() {
   const [selectedTask, setSelectedTask] = useAtom(selectedTaskAtom);
   const [, setTaskFlowsGenerate] = useAtom(taskFlowsGenerateAtom);
   const [filePreview, setFilePreview] = useState(null);
   const [exampleIndex, setExampleIndex] = useState(null);
+  const [fileName, setFileName] = useState("");
 
   // Local state for description
   const [localTaskDescription, setLocalTaskDescription] = useState(
@@ -47,12 +50,13 @@ function TaskPanel() {
     setLocalSelectedTask({ ...task, uploadedFile: null });
     setLocalTaskDescription(task.description || "");
     setFilePreview(null);
+    setFileName("");
   };
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (!file) return;
-
+    setFileName(file.name);
     const previewURL = URL.createObjectURL(file);
     setFilePreview(previewURL);
     setLocalSelectedTask((prev) => ({ ...prev, uploadedFile: file }));
@@ -60,6 +64,7 @@ function TaskPanel() {
 
   const handleFileDelete = () => {
     setFilePreview(null);
+    setFileName("");
     setLocalSelectedTask((prev) => ({ ...prev, uploadedFile: null }));
   };
 
@@ -100,93 +105,82 @@ function TaskPanel() {
   };
 
   return (
-    <Box sx={{ width: "100%", padding: 2 }}>
-      <h3> Define the Task!</h3>
-      {/* Description text field */}
-      <Grid container spacing={6}>
-        <Grid item xs={9}>
-          <TextField
-            variant="filled"
-            value={localTaskDescription}
-            onChange={handleInputChange}
-            placeholder="Enter your task description, or select one from the examples provided below."
-            minRows={2}
-            sx={{ width: "800px" }}
-          />
+    <Box
+      sx={{
+        width: "100%",
+        padding: 2,
+        display: "flex",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <TextField
+          variant="filled"
+          value={localTaskDescription}
+          onChange={handleInputChange}
+          placeholder="Enter your task description, or select one from the examples provided."
+          label="Task Description"
+          size="small"
+          minRows={2}
+          sx={{ width: "550px" }}
+        />
 
-          <Box
-            sx={{
-              mb: 2,
-              padding: 1,
-              marginBottom: 1,
-              marginTop: 1,
-              gap: 1,
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Button onClick={handleSubmit} color="primary" variant="outlined">
-              Submit Task
-            </Button>
+        {/* File Upload Button */}
+        <Button
+          component={fileName ? undefined : "label"}
+          color="primary"
+          size="small"
+          variant={fileName ? "contained" : "outlined"}
+          role={undefined}
+          startIcon={fileName ? <DeleteIcon /> : <CloudUploadIcon />}
+          onClick={fileName ? handleFileDelete : undefined}
+        >
+          {fileName || "Upload Files"}
+          {!fileName && (
+            <Input
+              type="file"
+              onChange={handleFileChange}
+              sx={{ display: "none" }}
+            />
+          )}
+        </Button>
 
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center", // Align label and select vertically
-                gap: 1, // Add spacing between label and select
-              }}
-            >
-              <InputLabel id="task-select-label">Example Tasks</InputLabel>
-              <Select
-                value={localSelectedTask.index ?? ""}
-                sx={{ minWidth: "200px", height: "50px", color: "#777" }}
-                variant="standard"
-                onChange={handleTaskSelect}
-              >
-                {taskList.map((task, index) => (
-                  <MenuItem key={task.id} value={index}>
-                    {task.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={3}>
-          {" "}
-          {/* conditional file upload */}
-          <Box>
-            <Typography sx={{ fontSize: "14px", color: "#333" }}>
-              Example Input (Optional)
-            </Typography>
-            <TextField></TextField>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              <Input
-                type="file"
-                onChange={handleFileChange}
-                sx={{ fontSize: "12px" }}
-              />
-              {filePreview && (
-                <>
-                  {/* <Typography sx={{ fontSize: '12px', textWrap: 'wrap'}}>
-                        {selectedTask.uploadedFile?.name}
-                      </Typography> */}
-                  <Button
-                    onClick={handleFileDelete}
-                    size="small"
-                    variant="contained"
-                    color="error"
-                    sx={{ fontSize: "10px", padding: "2px 6px" }}
-                  >
-                    X
-                  </Button>
-                </>
-              )}
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+        {/* Submit Button */}
+        <Button
+          onClick={handleSubmit}
+          color="primary"
+          variant="contained"
+          size="small"
+        >
+          Submit Task
+        </Button>
+      </Box>
+
+      {/* Example Task Selector */}
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <InputLabel id="task-select-label">Example Tasks</InputLabel>
+        <Select
+          value={localSelectedTask.index ?? ""}
+          sx={{ minWidth: "200px", height: "50px", color: "#777" }}
+          variant="standard"
+          onChange={handleTaskSelect}
+        >
+          {taskList.map((task, index) => (
+            <MenuItem key={task.id} value={index}>
+              {task.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </Box>
+
+      {/* <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Typography sx={{ fontSize: "14px", color: "#333" }}>
+          Example Input (Optional)
+        </Typography>
+
+        <TextField size="small" />
+      </Box> */}
     </Box>
   );
 }
