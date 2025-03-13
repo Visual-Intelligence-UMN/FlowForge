c5cde69ecdcff7ae3665b7f151c20c247a39ac18
Pan Hao <panzhan1@outlook.com>
2025-03-13 11:31:48 -0500

    refactor node

--- src/components/nodes/node-agents/SingleAgentNode.tsx
+++ src/components/nodes/node-agents/SingleAgentNode.tsx
@@ -4,48 +4,62 @@ import {TextField, Typography, Box, FormControl, InputLabel, Select, MenuItem, L
 
 const SingleAgentNode = ({ id, data }) => {
 
+  // console.log("data", data);
+
   const { updateNodeFieldset } = data;
+  const showContent = data.showContent;
+
+  // console.log("showContent", showContent);
 
   const onChange = (fieldName) => (event) => {
     updateNodeFieldset(id, fieldName, event.target.value);
   };
 
-  return (
-    <Box className="single-agent-node" sx={{p: 2, width: "260px"}}>
+  const handles = () => {
+    if (data.label.includes("Worker")) {
+      return (
+        <>
+        <Handle type="source" position={Position.Top} id={`top-${id}`} />
+        <Handle type="target" position={Position.Top} id={`top-${id}`} />
+        </>
+      )
+    } else if (data.label.includes("Agent")) {
+      return (
+        <>
+        <Handle type="target" position={Position.Left} id={`in-left-${id}`} />
+        <Handle type="source" position={Position.Right} id={`out-right-${id}`} />
+        </>
+      )
+    }
+  }
 
-      <Handle type="target" position={Position.Left} id={`in-left-${id}`} />
-      <Handle type="source" position={Position.Right} id={`out-right-${id}`} />
-      <Handle type="source" position={Position.Top} id={`top-${id}`} />
-      <Handle type="target" position={Position.Top} id={`top-${id}`} />
+  const modelSelector = () => {
+    return (
+      <Box mt={2} mb={2}>
+      <FormControl fullWidth>
+        <InputLabel id={`llm-label-${id}`}>LLM Model</InputLabel>
+        <Select
+          labelId={`llm-label-${id}`}
+          id={`llm-select-${id}`}
+          value={data.llm}
+          label="LLM Model"
+          onChange={onChange("llm")}
+          size="small"
+          className="nodrag nopan" // set to prevent dragging and panning
+        >
+          <MenuItem value="gpt-4o-mini">GPT-4o Mini</MenuItem>
+          <MenuItem value="gpt-3.5-turbo">GPT-3.5 Turbo</MenuItem>
+          <MenuItem value="gpt-4o">GPT-4o</MenuItem>
+          <MenuItem value="other">Other</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+      )
+  }
 
-
-      {"Step "+(Number(id.split("-")[1]))}
-      <Typography variant="h6" gutterBottom>
-        🤖 {data.label || "Single Agent"}
-      </Typography>
-
-       <Box mt={2} mb={2}>
-        <FormControl fullWidth>
-          <InputLabel id={`llm-label-${id}`}>LLM Model</InputLabel>
-          <Select
-            labelId={`llm-label-${id}`}
-            id={`llm-select-${id}`}
-            value={data.llm}
-            label="LLM Model"
-            onChange={onChange("llm")}
-            size="small"
-            className="nodrag nopan" // set to prevent dragging and panning
-          >
-            <MenuItem value="gpt-4o-mini">GPT-4o Mini</MenuItem>
-            <MenuItem value="gpt-3.5-turbo">GPT-3.5 Turbo</MenuItem>
-            <MenuItem value="gpt-4o">GPT-4o</MenuItem>
-            <MenuItem value="other">Other</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-
-      {/* System Prompt */}
-        <TextField
+  const systemPrompt = () => {
+    return(
+      <TextField
           label="System Prompt" 
           variant="outlined"
           multiline
@@ -56,9 +70,12 @@ const SingleAgentNode = ({ id, data }) => {
           onChange={onChange("systemPrompt")}
           className="nodrag nopan nowheel"
         />
-        
+    )
+  }
 
 
+  const toolsSelector = () => {
+    return(
       <Box mt={2} mb={2}>
         <FormControl fullWidth>
           <InputLabel id={`tools-label-${id}`}>Tools</InputLabel>
@@ -83,8 +100,26 @@ const SingleAgentNode = ({ id, data }) => {
           </Select>
         </FormControl>
       </Box>
+    )
+  }
 
-      {/* Multiple Handles for Outputs */}
+
+  return (
+    <Box className="single-agent-node" sx={{p: 2, width: "260px"}}>
+
+      {handles()}
+
+      {"Step "+(Number(id.split("-")[1]))}
+      <Typography variant="h6" gutterBottom>
+        🤖 {data.label || "Single Agent"}
+      </Typography>
+
+      {modelSelector()}
+
+      {systemPrompt()}
+
+      {toolsSelector()}
+
      
     </Box>
   );
